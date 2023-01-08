5f8b44143d47ed89c46b73595ef34bde5c0dadf5
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-01-08 07:17:36 +0600

    Real time data delete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import AddTask from "./Components/AddTask";
 import TaskList from "./Components/TaskList";
+
+export const DeleteHandlerContext = createContext();
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -23,12 +25,30 @@ const App = () => {
       console.log(error.message);
     }
   };
+
+  // delete event
+  const handleDelete = (id) => {
+    //delete data only
+    deleteData(id);
+    //set updated tasks
+    setTasks(tasks.filter((task) => id !== task.id));
+  };
+  const deleteData = async (id) => {
+    await fetch(`https://imminent-blush-haumea.glitch.me/task/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  };
   return (
     <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex flex-col py-10">
-      <Header />
-      <AddTask tasks={tasks} setTasks={setTasks} />
-      <TaskList tasks={tasks} />
-      <Footer />
+      <DeleteHandlerContext.Provider value={handleDelete}>
+        <Header />
+        <AddTask tasks={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} />
+        <Footer />
+      </DeleteHandlerContext.Provider>
     </div>
   );
 };
--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -1,7 +1,10 @@
 import { FiEdit, FiTrash } from "react-icons/fi";
-import React from "react";
+import React, { useContext } from "react";
+import { DeleteHandlerContext } from "../App";
 
 const TaskItem = ({ task }) => {
+  const handleDelete = useContext(DeleteHandlerContext);
+
   return (
     <div className="task-item flex justify-between items-center bg-gray-800 p-5 rounded hover:bg-gradient-to-r hover:from-teal-800 hover:to-gray-800 group">
       <div className="task-item-left flex gap-3">
@@ -14,9 +17,9 @@ const TaskItem = ({ task }) => {
         <span>
           <FiEdit className=" text-gray-500 hover:text-teal-500 cursor-pointer duration-300" />
         </span>
-        <span>
+        <button onClick={() => handleDelete(task.id)}>
           <FiTrash className=" text-gray-500 hover:text-red-500 cursor-pointer duration-300" />
-        </span>
+        </button>
       </div>
     </div>
   );
